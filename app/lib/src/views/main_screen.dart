921415eb1a87b22845c13b8d2a1f5f40a553db24
ppe015 <daisy.johnson@student.uib.no>
2021-11-05 10:34:34 +0100

    Fixed color sheme and added all necessary

--- app/lib/src/views/main_screen.dart
+++ app/lib/src/views/main_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import '../auth/auth_service.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
-import 'assets/theme.dart' as color;
+import 'Style/colors.dart' as color;
 
 class MainScreen extends StatefulWidget {
   @override
@@ -15,19 +15,22 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     AuthService authService = context.watch<AuthService>();
-
     return Scaffold(
-      appBar: AppBar(
-          leading: Image.asset('assets/EPR.png', fit: BoxFit.scaleDown),
-          title: Text("Early Pregnancy Risk", style: TextStyle(fontSize: 24)),
-          actions: [
-            GoogleSignInButton(
-              onPressed: () {
-                authService.signInWithGoogle();
-              },
-            ),
-            Text(authService.errorMessage),
-          ]),
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(75.0),
+          child: AppBar(
+            leading:
+                Image.asset('assets/images/EPR.png', fit: BoxFit.scaleDown),
+            title: Text("Early Pregnancy Risk", style: TextStyle(fontSize: 24)),
+            actions: [
+              GoogleSignInButton(
+                onPressed: () {
+                  authService.signInWithGoogle();
+                },
+              ),
+              Text(authService.errorMessage),
+            ],
+          )),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
