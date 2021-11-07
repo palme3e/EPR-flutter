3a2910d2c48f7816b3f846e2e5485ea34f796aae
ppe015 <daisy.johnson@student.uib.no>
2021-11-07 18:15:24 +0100

    Continued on questions

--- app/lib/src/views/factors.dart
+++ app/lib/src/views/factors.dart
@@ -1,5 +1,7 @@
 import 'dart:html';
 
+import 'package:flutter/services.dart';
+
 import '../networking/requests.dart' as request;
 import 'package:app/src/auth/auth_service.dart';
 import 'package:flutter/material.dart';
@@ -9,21 +11,23 @@ import '../auth/auth_service.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
 import 'Style/colors.dart' as color;
 
+final question_list = request.get_factors("en");
 //Function to check if bool or int type question
 check_factor_type(Map factor) {
   String type = null;
-  if (factor["answertype"] == "boolean") {
+  var temp = factor[0];
+  if (temp["answertype"] == "boolean") {
     type = "bool";
-  } else if (factor["answertype"] == "integer") {
+  } else if (temp["answertype"] == "integer") {
     type = "int";
   }
-  List<String> question_type = [factor["question"], type];
+  List<String> question_type = [temp["question"], type];
   return question_type;
 }
 
-get_factors() {
-  List<Map> factors = request.get_factors("en");
-  return factors;
+get_question(int counter) {
+  print(question_list);
+  return null;
 }
 
 class Factors extends StatefulWidget {
@@ -36,7 +40,25 @@ class _MainScreenState extends State<Factors> {
   Widget build(BuildContext context) {
     AuthService authService = context.watch<AuthService>();
     return Scaffold(
-      body: Center(child: Text(check_factor_type(get_factors()[0])[0])),
-    );
+        body: Center(
+            child: FutureBuilder<String>(
+      future: get_question(0),
+      builder: (
+        BuildContext context,
+        AsyncSnapshot<String> snapshot,
+      ) {
+        if (snapshot.hasData) {
+          return Text(
+            snapshot.data ?? "default filler",
+            style: const TextStyle(color: Colors.black, fontSize: 20),
+            softWrap: true,
+          );
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+        // By default, show a loading spinner.
+        return const CircularProgressIndicator();
+      },
+    )));
   }
 }
