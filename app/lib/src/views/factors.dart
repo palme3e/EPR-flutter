4a76072677f2fcaf8d5d322cf97828931856e2b9
ppe015 <daisy.johnson@student.uib.no>
2021-11-08 16:06:02 +0100

    Added appbar and padding to factors.dart

--- app/lib/src/views/factors.dart
+++ app/lib/src/views/factors.dart
@@ -8,6 +8,7 @@ import 'package:provider/provider.dart';
 import '../auth/auth_service.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
 import 'Style/colors.dart' as color;
+import 'package:app/src/views/app_bar.dart';
 
 check_factor_type(Map factor) {
   String type = null;
@@ -39,54 +40,62 @@ class _MainScreenState extends State<Factors> {
     AuthService authService = context.watch<AuthService>();
     int index = 0;
     return Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(75.0),
+          child: topBar(context, authService),
+        ),
         body: Center(
-            child: FutureBuilder<List>(
-          future: question_list,
-          builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
-            if (snapshot.hasData) {
-              List myMap = snapshot.data;
-              print("før, ");
-              print(index);
-              return Text(myMap[index]["question"],
-                  style: TextStyle(fontSize: 25, color: color.darkblue));
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
-            return const CircularProgressIndicator();
-          },
-        )),
-        floatingActionButton: Padding(
-          padding: EdgeInsets.fromLTRB(550, 0, 500, 300),
-          child: Row(
-            children: [
-              TextButton(
-                  style: TextButton.styleFrom(
-                    padding: const EdgeInsets.all(16.0),
-                    primary: Colors.blue,
-                    textStyle: const TextStyle(fontSize: 20),
-                  ),
-                  onPressed: () {
-                    index = update_index(index);
-                    print("etter, ");
-                    print(index);
-                  },
-                  child: const Text('No')), //TODO change name
-
-              TextButton(
-                style: TextButton.styleFrom(
-                  padding: const EdgeInsets.all(16.0),
-                  primary: Colors.blue,
-                  textStyle: const TextStyle(fontSize: 20),
-                ),
-                onPressed: () {
-                  index = update_index(index);
-                  print("etter, ");
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            FutureBuilder<List>(
+              future: question_list,
+              builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+                if (snapshot.hasData) {
+                  List myMap = snapshot.data;
+                  print("før, ");
                   print(index);
-                },
-                child: const Text('Yes'), //TODO change name
-              )
-            ],
-          ),
+                  return Text(myMap[index]["question"],
+                      style: TextStyle(fontSize: 25, color: color.darkblue));
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+                return const CircularProgressIndicator();
+              },
+            ),
+            Center(
+              child: Row(
+                children: [
+                  Spacer(),
+                  TextButton(
+                      style: TextButton.styleFrom(
+                        //padding: const EdgeInsets.all(16.0),
+                        primary: Colors.blue,
+                        textStyle: const TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {
+                        index = update_index(index);
+                        print("etter, ");
+                        print(index);
+                      },
+                      child: const Text('No')), //TODO change name
+
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      //padding: const EdgeInsets.all(16.0),
+                      primary: Colors.blue,
+                      textStyle: const TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {
+                      index = update_index(index);
+                      print("etter, ");
+                      print(index);
+                    },
+                    child: const Text('Yes'), //TODO change name
+                  ),
+                  Spacer()
+                ],
+              ),
+            )
+          ]),
         ));
   }
 }
