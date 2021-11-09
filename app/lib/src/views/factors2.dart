8bb0e16e2d76209192d04570c21ff0d4997ad0c5
ppe015 <daisy.johnson@student.uib.no>
2021-11-09 14:03:42 +0100

    Styling for the questions

--- app/lib/src/views/factors2.dart
+++ app/lib/src/views/factors2.dart
@@ -64,42 +64,59 @@ class _QuestionsState extends State<Questions> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(_questions.length > 0
-            ? _questions[_indexQuestion]["question"]
-            : "Error"),
-        Container(
-            color: Colors.white,
-            child: (check_factor_type(_questions[_indexQuestion])) //bool = true
-                ? Column(children: [
-                    TextButton(
-                        style: TextButton.styleFrom(
-                          //padding: const EdgeInsets.all(16.0),
-                          primary: Colors.blue,
-                          textStyle: const TextStyle(fontSize: 20),
-                        ),
-                        onPressed: (_next),
-                        child: (Text("Yes"))),
-                    TextButton(
-                        style: TextButton.styleFrom(
-                          //padding: const EdgeInsets.all(16.0),
-                          primary: Colors.blue,
-                          textStyle: const TextStyle(fontSize: 20),
-                        ),
-                        onPressed: (_next),
-                        child: (Text("No")))
-                  ])
-                : TextButton(
-                    style: TextButton.styleFrom(
-                      //padding: const EdgeInsets.all(16.0),
-                      primary: Colors.blue,
-                      textStyle: const TextStyle(fontSize: 20),
-                    ),
-                    onPressed: (_next),
-                    child: (Text("Int"))))
-      ],
+    AuthService authService = context.watch<AuthService>();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(75.0),
+        child: topBar(context, authService),
+      ),
+      body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text(
+            _questions.length > 0
+                ? _questions[_indexQuestion]["question"]
+                : "Error",
+            style: TextStyle(fontSize: 25, color: color.darkblue)),
+        Center(
+          child: (check_factor_type(_questions[_indexQuestion])) //bool = true
+              ? Row(children: [
+                  Spacer(),
+                  TextButton(
+                      style: TextButton.styleFrom(
+                        primary: Colors.blue,
+                        textStyle: const TextStyle(fontSize: 20),
+                      ),
+                      onPressed: (_next),
+                      child: (Text("No"))),
+                  TextButton(
+                      style: TextButton.styleFrom(
+                        //padding: const EdgeInsets.all(16.0),
+                        primary: Colors.blue,
+                        textStyle: const TextStyle(fontSize: 20),
+                      ),
+                      onPressed: (_next),
+                      child: (Text("Yes"))),
+                  Spacer()
+                ])
+              : TextButton(
+                  style: TextButton.styleFrom(
+                    //padding: const EdgeInsets.all(16.0),
+                    primary: Colors.blue,
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  onPressed: (_next),
+                  child: (Text("Int"))),
+        )
+      ])),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => MainScreen()));
+        },
+        child: const Icon(Icons.play_arrow),
+        backgroundColor: Colors.blue,
+      ),
     );
   }
 }
