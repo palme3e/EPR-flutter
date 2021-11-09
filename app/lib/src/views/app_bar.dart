c4a9adeaa39d87ab71443e66ba11871889d8e2e5
KjerstiDA <kjerstiaslesen@gmail.com>
2021-11-09 19:37:42 +0100

    Adjusted sign out button alignment

--- app/lib/src/views/app_bar.dart
+++ app/lib/src/views/app_bar.dart
@@ -9,82 +9,45 @@ AppBar topBar(BuildContext context, AuthService authService) =>
 AppBar(
   toolbarHeight: 75,
   leadingWidth: 75,
-  leading:
-
-    Padding(
-      padding: EdgeInsets.only(left: 10),
-      child:
-      Align(
-        alignment: Alignment(0,0),
-        child: Image.asset('assets/images/EPR.png', fit: BoxFit.fitHeight)
+  leading:Padding(
+    padding: EdgeInsets.only(left: 10),
+    child:Align(
+      alignment: Alignment(0,0),
+      child: Image.asset('assets/images/EPR.png', fit: BoxFit.fitHeight)
     )
-    ),
+  ),
   title: Align(
     alignment: Alignment.centerLeft,
-    child: Text("Early Pregnancy Risk", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),)
-      ,
-      actions:  [
-        if (authService.status == Status.Unauthenticated)
-        Align(
-          alignment: Alignment.center,
-          child: Padding(padding: EdgeInsets.only(right: 25),
-          child: GoogleSignInButton(
-            darkMode: true,
-            onPressed: () {
-              authService.signInWithGoogle();
-            },
-          ),
-          ) ,
+    child: Text("Early Pregnancy Risk", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+  ),
+  actions: [
+    if (authService.status == Status.Unauthenticated)
+    Align(
+      alignment: Alignment.center,
+      child: Padding(
+        padding: EdgeInsets.only(right: 25),
+        child: GoogleSignInButton(
+          darkMode: true,
+          onPressed: () {
+            authService.signInWithGoogle();
+          },
+        ),
+      ),
+    ),
+    Text(authService.errorMessage),
+    if (authService.status == Status.Authenticated)
+    Align(
+      alignment: Alignment.center,
+      child: Padding(
+        padding: EdgeInsets.only(right: 25),
+        child: MaterialButton(
+          child: Text("Sign out"),
+          onPressed: () async {
+            await authService.signOut();
+          },
         )
-          ,
-        Text(authService.errorMessage),
-        if (authService.status == Status.Authenticated)
-        
-          MaterialButton(
-            child: Text("Sign out"),
-            onPressed: () async {
-              await authService.signOut();
-            },
-          )
-      ],
+      )
+    )
+  ],
 );
   
-
-
-/**
- *  
- * toolbarHeight: 75,
-  leading: 
-    Align(
-      alignment: Alignment(0,0),
-      child:Image.asset('assets/images/EPR.png', fit: BoxFit.scaleDown)
-    ),
-  title: Align(
-    alignment: Alignment.centerLeft,
-    child: Text("Early Pregnancy Risk", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),)
-      ,
-      actions:  [
-        if (authService.status == Status.Unauthenticated)
-        Align(
-          alignment: Alignment.center,
-          child: Padding(padding: EdgeInsets.only(right: 25),
-          child: GoogleSignInButton(
-            darkMode: true,
-            onPressed: () {
-              authService.signInWithGoogle();
-            },
-          ),
-          ) ,
-        )
-          ,
-        Text(authService.errorMessage),
-        if (authService.status == Status.Authenticated)
-        
-          MaterialButton(
-            child: Text("Sign out"),
-            onPressed: () async {
-              await authService.signOut();
-            },
-          )
-      ],
- */
