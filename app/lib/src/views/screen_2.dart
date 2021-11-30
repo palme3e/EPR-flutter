1c5ea8c4fa7b41bab8643bd5bd9ef8b3f96288e8
ppe015 <daisy.johnson@student.uib.no>
2021-11-30 11:26:01 +0100

    Fixed error msg

--- app/lib/src/views/screen_2.dart
+++ app/lib/src/views/screen_2.dart
@@ -55,72 +55,86 @@ class _Screen2State extends State<Screen2> {
     AuthService authService = context.watch<AuthService>();
     return Scaffold(
         appBar: topBar(context, authService),
-        body: Column(children: [
-          Padding(
-              padding: EdgeInsets.fromLTRB(75, 100, 75, 0),
-              child:
-                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                Text(_texts.length > 0 ? _texts["intro_complication"] : " ",
-                    style: const TextStyle(color: Colors.black, fontSize: 20))
-              ])),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                children: [
-                  for (var i = 0;
-                      i < _texts["complication_list_1"].split(";").length;
-                      i++)
-                    TextButton(
-                      style: TextButton.styleFrom(
-                        primary: Colors.blue,
-                        textStyle: TextStyle(color: Colors.black, fontSize: 16),
-                      ),
-                      onPressed: () {
-                        _launch_URL(
-                            _texts["complication_list_1"].split(";")[i]);
-                      },
-                      child: Text(_texts["complication_list_1"].split(";")[i]),
-                    )
-                ],
-              ),
-              Column(
-                children: [
-                  for (var i = 0;
-                      i < _texts["complication_list_2"].split(";").length;
-                      i++)
-                    TextButton(
-                      style: TextButton.styleFrom(
-                        primary: Colors.blue,
-                        textStyle: TextStyle(color: Colors.black, fontSize: 16),
-                      ),
-                      onPressed: () {
-                        _launch_URL(
-                            _texts["complication_list_2"].split(";")[i]);
-                      },
-                      child: Text(_texts["complication_list_2"].split(";")[i]),
+        body: _texts.length > 0
+            ? Column(children: [
+                Padding(
+                    padding: EdgeInsets.fromLTRB(75, 100, 75, 0),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                              _texts.length > 0
+                                  ? _texts["intro_complication"]
+                                  : " ",
+                              style: const TextStyle(
+                                  color: Colors.black, fontSize: 20))
+                        ])),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        for (var i = 0;
+                            i < _texts["complication_list_1"].split(";").length;
+                            i++)
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              primary: Colors.blue,
+                              textStyle:
+                                  TextStyle(color: Colors.black, fontSize: 16),
+                            ),
+                            onPressed: () {
+                              _launch_URL(
+                                  _texts["complication_list_1"].split(";")[i]);
+                            },
+                            child: Text(
+                                _texts["complication_list_1"].split(";")[i]),
+                          )
+                      ],
                     ),
-                ],
-              )
-            ],
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(75, 100, 75, 0),
-            child: Column(
-              children: [
-                Text(_texts.length > 0 ? _texts["note_that"] : " ",
-                    style: const TextStyle(color: Colors.black, fontSize: 20)),
-                Text(
-                  _texts.length > 0 ? _texts["warning_text_page_2"] : " ",
-                  style: const TextStyle(color: Colors.black, fontSize: 15),
-                  textAlign: TextAlign.center,
+                    Column(
+                      children: [
+                        for (var i = 0;
+                            i < _texts["complication_list_2"].split(";").length;
+                            i++)
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              primary: Colors.blue,
+                              textStyle:
+                                  TextStyle(color: Colors.black, fontSize: 16),
+                            ),
+                            onPressed: () {
+                              _launch_URL(
+                                  _texts["complication_list_2"].split(";")[i]);
+                            },
+                            child: Text(
+                                _texts["complication_list_2"].split(";")[i]),
+                          ),
+                      ],
+                    )
+                  ],
                 ),
-                Text(_texts.length > 0 ? _texts["explanation_text"] : " ",
-                    style: const TextStyle(color: Colors.black, fontSize: 15)),
-              ],
-            ),
-          )
-        ]),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(75, 100, 75, 0),
+                  child: Column(
+                    children: [
+                      Text(_texts.length > 0 ? _texts["note_that"] : " ",
+                          style: const TextStyle(
+                              color: Colors.black, fontSize: 20)),
+                      Text(
+                        _texts.length > 0 ? _texts["warning_text_page_2"] : " ",
+                        style:
+                            const TextStyle(color: Colors.black, fontSize: 15),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(_texts.length > 0 ? _texts["explanation_text"] : " ",
+                          style: const TextStyle(
+                              color: Colors.black, fontSize: 15)),
+                    ],
+                  ),
+                )
+              ])
+            : Loading(),
         floatingActionButton:
             Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
           Padding(
